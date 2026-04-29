5d18a101d8943f63356fed4a99fb89e07d0047a6
shiudzaomiao <1099156042@qq.com>
2026-04-29 16:07:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url' // 新增路径处理
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 🔥 就加这一行！部署 GitHub Pages 必需
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,8 +16,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5999',
         changeOrigin: true,
-        // 如果后端服务不稳定，可以尝试 127.0.0.1
-        // target: 'http://127.0.0.1:5999',
         configure: (proxy, options) => {
           proxy.on('error', (err, req, res) => {
             console.log('Proxy Error:', err);
